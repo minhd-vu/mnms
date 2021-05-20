dd3652005b67e8f18c8873127494d10574a3feb4
Minh Vu <minhd_vu@yahoo.com>
2021-05-20 14:39:01 -0400

    update produces annotations

--- src/main/java/mnms/HelloController.java
+++ src/main/java/mnms/HelloController.java
@@ -8,17 +8,20 @@ import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Produces;
 
 @Controller("/hello")
 public class HelloController {
 
-    @Get(produces = MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get
     public String index() {
         return "Hello World";
     }
     
     @SuppressWarnings("unchecked")
-    @Post(produces = MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Post
     public String helloPost(@Body String body) {
         JSONParser parser = new JSONParser();
 
