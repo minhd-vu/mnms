3541888e4260db77feb3915950422512388f6cec
Minh Vu <minhd_vu@yahoo.com>
2021-06-01 11:33:33 -0400

    update application.yml

--- src/main/java/mnms/MessageStream.java
+++ src/main/java/mnms/MessageStream.java
@@ -5,29 +5,24 @@ import io.micronaut.context.annotation.Factory;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
+
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 @Factory
 public class MessageStream {
-    public static final String STREAM_MESSAGE_COUNT = "message-stream";
+    public static final String STREAM_MESSAGE = "message-stream";
     public static final String INPUT = "message-topic";
     public static final String OUTPUT = "message-topic-output";
 
     @Singleton
-    @Named(STREAM_MESSAGE_COUNT)
+    @Named(STREAM_MESSAGE)
     KStream<Long, Message> messageStream(ConfiguredStreamBuilder builder) {
         Properties props = builder.getConfiguration();
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
@@ -37,5 +32,4 @@ public class MessageStream {
 
         return source;
     }
-
 }
