5d0b59a8226ac158540ecf183e54eb7c2ec6031f
Minh Vu <minhd_vu@yahoo.com>
2021-06-02 17:02:48 -0400

    added openapi

--- src/main/java/mnms/Application.java
+++ src/main/java/mnms/Application.java
@@ -1,7 +1,12 @@
 package mnms;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(info = @Info(title = "mnms", version = "0.0", description = "Micronaut Microservice", license = @License(name = "Apache 2.0", url = "http://localhost:8080"), contact = @Contact(url = "sas.com", name = "Minh", email = "minh.vu@sas.com")))
 public class Application {
 
     public static void main(String[] args) {
