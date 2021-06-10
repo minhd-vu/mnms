25107a4438dde9d23679bb34d7da3f4ebf5d5031
Minh Vu <minhd_vu@yahoo.com>
2021-06-10 10:39:23 -0400

    added metrics

--- src/main/java/mnms/MessageListener.java
+++ src/main/java/mnms/MessageListener.java
@@ -7,7 +7,7 @@ import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.OffsetReset;
 import io.micronaut.configuration.kafka.annotation.Topic;
 
-@KafkaListener(offsetReset = OffsetReset.EARLIEST, threads = 10)
+@KafkaListener(offsetReset = OffsetReset.EARLIEST, groupId = "message.listener", threads = 10)
 public class MessageListener {
     @Inject
     MessageRepository messageRepository;
